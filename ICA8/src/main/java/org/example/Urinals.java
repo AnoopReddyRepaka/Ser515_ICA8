e2402326053500c29705ff5a2b149155d744f79c
AnoopReddyRepaka <113136143+AnoopReddyRepaka@users.noreply.github.com>
2023-03-01 22:57:16 -0700

    Readin_TooLong passes

--- ICA8/src/main/java/org/example/Urinals.java
+++ ICA8/src/main/java/org/example/Urinals.java
@@ -6,7 +6,10 @@ public class Urinals {
     {
        urinals=input;
        if(input==""||input.length()>20) return false;
-
+       for(char curr:input.toCharArray())
+       {
+           if(curr!=0||curr!=1) return false;
+       }
 
        return true;
     }
